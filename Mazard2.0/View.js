a6dd0bb7a7b1343bf645bbc480851bf38ea7adbf
Unknown <bburkhardt5@gmail.com>
2017-09-02 00:24:07 -0600

    Auto stash before merge of "master" and "origin/master"

--- Mazard2.0/View.js
+++ Mazard2.0/View.js
@@ -5,10 +5,9 @@ function genGameBoard() {
     for (var i = 0; i < gameBoardSize.row; i++) {
         html += "<tr>";
         for (var j = 0; j < gameBoardSize.col; j++) {
-            html += "<td id =" + i + "," + j + "></td>"
+            html += "<td id =" + i + "," + j + " style='background-image: url(" + tileObjects.blankTile.image + ")' ></td>"
         }
         html += "</td>";
     }
     gameBoard.innerHTML = html;
-
 }
